6743ff8fd088278b3d20ba5d8eb7d0dbffff5a4a
Shangyi Luo <68954447+BioinfoDriver@users.noreply.github.com>
2021-08-03 14:51:18 +0800

    Update analysed_data_setup.R

--- code/1.Widespread_intra-tumoral_SCNA_heterogeneity/analysed_data_setup.R
+++ code/1.Widespread_intra-tumoral_SCNA_heterogeneity/analysed_data_setup.R
@@ -1,12 +1,12 @@
 
 # gold set
-gold.set <- readRDS( file='/pub5/xiaoyun/Jobs/J22/CopyNumberClonalityProject/Resource/CuratedData/gold_set.rds')
+gold.set <- readRDS( file='/data/gold_set.rds')
 
-pan.glio.cli.mol.data <- readRDS(file='/pub5/xiaoyun/Jobs/J22/CopyNumberClonalityProject/Resource/CuratedData/tcga_glioma_cli_mol.rds')
-gli.cn.alt.frac <- readRDS(file='/pub5/xiaoyun/Jobs/J22/CopyNumberClonalityProject/Resource/CuratedData/tcga_gli_cn_alt_frac.rds') 
+pan.glio.cli.mol.data <- readRDS(file='/data/tcga_glioma_cli_mol.rds')
+gli.cn.alt.frac <- readRDS(file='/data/tcga_gli_cn_alt_frac.rds') 
 
 gli.cn.alt.frac <- cbind(gli.cn.alt.frac[paste0(gold.set, '-01'), ], pan.glio.cli.mol.data[gold.set, ])
-saveRDS(gli.cn.alt.frac, file='/pub5/xiaoyun/Jobs/J22/CopyNumberClonalityProject/Resource/CuratedData/gli_glod_cn_alt_frac.rds')
+saveRDS(gli.cn.alt.frac, file='/data/gli_glod_cn_alt_frac.rds')
 
 
 
