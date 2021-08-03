9aaefc8ed6e7229d7f464b615e065cf79cc42779
Shangyi Luo <68954447+BioinfoDriver@users.noreply.github.com>
2021-08-03 20:06:14 +0800

    Update rtks_het_plot.R

--- code/4.RTK_co-amplifications_tend_to_be_subclonal/rtks_het_plot.R
+++ code/4.RTK_co-amplifications_tend_to_be_subclonal/rtks_het_plot.R
@@ -1,12 +1,11 @@
 
 ############################################Receptor tyrosine kinases
-setwd('/pub5/xiaoyun/Jobs/J22/CopyNumberClonalityProject/Results/Section3/Resources')
-rtks <- read.csv(file='RTKs.txt', sep='\t', header=TRUE, stringsAsFactors=FALSE)
+rtks <- read.csv(file='/data/RTKs.txt', sep='\t', header=TRUE, stringsAsFactors=FALSE)
 rtks$HGNC.ID <- gsub(pattern='HGNC:', replacement='', rtks$HGNC.ID)
 
 
-gold.set <- readRDS('/pub5/xiaoyun/Jobs/J22/CopyNumberClonalityProject/Resource/CuratedData/gold_set.rds')
-gene.het <- readRDS(file='/pub5/xiaoyun/Jobs/J22/CopyNumberClonalityProject/Results/Section3/Resources/gene.het.5.0.rds')
+gold.set <- readRDS('/data/gold_set.rds')
+gene.het <- readRDS(file='/data/gene.het.rds')
 
 wrtk.het <- gene.het[rtks$Approved.symbol, ]
 wrtk.het <- wrtk.het[, paste(gold.set, '01', sep = '-')]
@@ -15,7 +14,7 @@ wrtk.het <- wrtk.het[, colSums(abs(wrtk.het))!=0]
 
 
 # annotation
-gli.cli.mol.data <- readRDS(file='/pub5/xiaoyun/Jobs/J22/CopyNumberClonalityProject/Resource/CuratedData/tcga_glioma_cli_mol.rds')
+gli.cli.mol.data <- readRDS(file='/data/tcga_glioma_cli_mol.rds')
 rownames(gli.cli.mol.data) <- paste(rownames(gli.cli.mol.data), '01', sep = '-')
 
 # gli.cli.mol.data <- subset(gli.cli.mol.data, histological_grade %in% ('G4'))
@@ -47,7 +46,7 @@ ha = HeatmapAnnotation(df = gli.cli.mol.data[, 'IDH_CODEL_SUBTYPE', FALSE], col 
  show_legend = FALSE, show_annotation_name = FALSE)
 
 
-pdf('/pub5/xiaoyun/Jobs/J22/CopyNumberClonalityProject/Results/Section3/Results/RTKClonalityPlot/all_rtk_plot.5.0.pdf')
+pdf('/result/all_rtk_plot.pdf')
  het.col <- c("#FC9272", "#CB181D", "#F7F7F7", "#084594", "#9ECAE1")
  names(het.col) <- c(2, 1, 0, -1, -2)
  Heatmap(matrix = wrtk.het, cluster_columns = FALSE, cluster_rows = FALSE, col = het.col, 
